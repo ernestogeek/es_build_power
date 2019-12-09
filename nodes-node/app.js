117ac4a68f373d51f468f7e2df5619fa04ffd110
ernestogeek <ernestodev12@gmail.com>
2019-12-09 23:42:01 +0100

    Using 3rd package nodemon

--- nodes-node/app.js
+++ nodes-node/app.js
@@ -1,37 +1,15 @@
 console.log('Starting app.');
-//File stream
-//Follow the link to understand require, modules and modules.exports
-//https://viblo.asia/p/nodejs-require-exports-and-moduleexports-djeZ18PQKWz
-//We will referent to a module and save it in a variable 'fs'
 
 const fs = require('fs');
-const os = require('os');
-const notes = require('./notes.js');
 const _ = require('lodash');
+
+const notes = require('./notes.js');
+
 
 console.log(_.isString(true));
 console.log(_.isString('Duy'));
+//_.uniq: Get the unique item in the array
 console.log(_.uniq('Tien-Duy'));
-var filteredArray = _.uniq(['TienDuy',1,'Tien','Duy','TienDuy','1','2',1,2]);
+var filteredArray = _.uniq(['TienDuy',1,'TienDuy','Duy','TienDuy',1,2,1,2]);
 console.log(filteredArray);
-
-
-
-// var user = os.userInfo();
-// console.log(user);
-
-// fs.appendFile('greetings.txt', '\nHello ' + user.username, (err) => {
-// 	if (err) throw err;
-// 	console.log('The "Hello world!" was appended to file!');
-// });
-
-// fs.appendFile('greetings.txt', `\nHello ${user.username} with age = ${notes.age}`, (err) => {
-// 	if (err) throw err;
-// 	console.log('The "Hello world!" was appended to file!');
-// });
-
-
-// var res = notes.addNote();
-// console.log(res);
-
-// console.log('Result:', notes.add(110,39));
+//Using Ctrl+C to stop nodemon
