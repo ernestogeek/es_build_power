32c37bf4a6f7aa2bfea157803c013b7bfdb087a1
ernestogeek <ernestodev12@gmail.com>
2019-12-09 22:35:08 +0100

    Using require

---
+++ nodes-node/app.js
@@ -0,0 +1,21 @@
+console.log('Starting app.');
+//File stream
+//Follow the link to understand require, modules and modules.exports
+//https://viblo.asia/p/nodejs-require-exports-and-moduleexports-djeZ18PQKWz
+//We will referent to a module and save it in a variable 'fs'
+
+const fs = require('fs');
+const os = require('os');
+
+var user = os.userInfo();
+console.log(user);
+
+fs.appendFile('greetings.txt', '\nHello ' + user.username, (err) => {
+	if (err) throw err;
+	console.log('The "Hello world!" was appended to file!');
+});
+
+fs.appendFile('greetings.txt', `\nHello ${user.username}`, (err) => {
+	if (err) throw err;
+	console.log('The "Hello world!" was appended to file!');
+});
