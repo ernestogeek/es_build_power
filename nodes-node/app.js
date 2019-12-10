e6eef3d76d0a33279fc83d58fce03a3b53cf7964
ernestogeek <ernestodev12@gmail.com>
2019-12-10 20:23:14 +0100

    Reading notes

--- nodes-node/app.js
+++ nodes-node/app.js
@@ -7,19 +7,17 @@ const notes = require('./notes.js');
 
 const argv = yargs.argv;
 var command = process.argv[2];
-console.log('Command: ',command);
+console.log('Command: ', command);
 console.log(process.argv);
-console.log('Yargs',argv);
+console.log('Yargs', argv);
 
-switch (command){
+switch (command) {
 	case 'add':
 		var note = notes.addNote(argv.title, argv.body);
-		if(note){
-			console.log('Node created');
-			console.log('__');
-			console.log(`Title: ${note.title}`);
-			console.log(`Body: ${note.body}`);
-		}else{
+		if (note) {
+			console.log('Note created');
+			logNote(note);
+		} else {
 			console.log('Node title taken');
 		}
 		break;
@@ -27,15 +25,27 @@ switch (command){
 		notes.getAll();
 		break;
 	case 'read':
-		notes.readNote(argv.title);
+		var note = notes.readNote(argv.title);
+		if (note) {
+			console.log('Node found');
+			logNote(note);
+		} else {
+			console.log('Node not found');
+		}
 		break;
 	case 'remove':
 		console.log(JSON.stringify(notes));
-		var noteRemoved =  notes.removeNote(argv.title);
-		var message = noteRemoved? 'Note was remove' : 'Note not found';
+		var noteRemoved = notes.removeNote(argv.title);
+		var message = noteRemoved ? 'Note was remove' : 'Note not found';
 		console.log(message);
 		break;
 	default:
 		console.log('Command note recognize');
 		break;
+}
+
+function logNote(note) {
+	console.log('__');
+	console.log(`Title: ${note.title}`);
+	console.log(`Body: ${note.body}`);
 }
--- nodes-node/notes.js
+++ nodes-node/notes.js
@@ -10,11 +10,11 @@ var fetchNode = () => {
 		return [];
 	}
 
-}
+};
 
 var saveNotes = (notes) => {
 	fs.writeFileSync('notes-data.json', JSON.stringify(notes));
-}
+};
 var addNote = (title, body) => {
 	var notes = fetchNode();
 	var note = {
@@ -32,21 +32,29 @@ var addNote = (title, body) => {
 var getAll = () => {
 	console.log('Getting all notes');
 };
+
 var readNote = (title) => {
-	console.log('Reading note', title);
+	var notes = fetchNode();
+	var filteredNode = notes.filter((note) => note.title === title);
+	return filteredNode[0];
 };
+
 var removeNote = (title) => {
 	var notes = fetchNode();
 	var filteredNode = notes.filter((note) => note.title !== title);
 	saveNotes(filteredNode);
 	return notes.length !== filteredNode.length;
 };
+
+
+
 module.exports = {
 	addNote,
 	getAll,
 	readNote,
 	removeNote
 }
+
 
 // node app.js add --title=secret --body="This is my secret"
 // node app.js list
