6755d9b4be9e4e8c6caa790969760a16f9f0e02d
ernestogeek <ernestodev12@gmail.com>
2019-12-16 21:43:12 +0100

    Dynamic page with handle bars

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -2,22 +2,53 @@ const express = require('express');
 const path= require('path');
 
 
-const publicDir = path.join(__dirname,'../public')
+const publicDir = path.join(__dirname,'../public');
+const viewDirPath = path.join(__dirname,'../views');
+const viewIndexFile = path.join(viewDirPath,'index.hbs');
+const viewAboutFile = path.join(viewDirPath,'about.hbs');
+const viewHelpFile = path.join(viewDirPath,'help.hbs');
 
 const app = express();
+
+//Using handlebars
+app.set('view engine','hbs');
 
 //use the static file from a folder
 app.use(express.static(publicDir));
 
-//hander ball: create a dynamic web
+//handle bars: create a dynamic web
 // --Reuse code
 // --create website and render easier
 
+//Using express nomal to create a new page
+// app.get('',(req,res) =>{
+//     res.send('<h1>Weather</h1>');
+// })
 
-app.get('',(req,res) =>{
-    res.send('<h1>Weather</h1>');
-})
 
+//Now we use handle bars to make it easier
+app.get('',(req,res)=>{
+    res.render(viewIndexFile,{
+        title:'TienDuy',
+        name:'Huong'
+    });
+});
+
+app.get('/about',(req,res)=>{
+    res.render(viewAboutFile,{
+        title: 'About me',
+        about:'Tien Duy is a Civil Engineer. He is now working for Vinci Construction France Company in Paris.',
+        name: 'Tien Duy  NGUYEN'
+    });
+});
+
+
+app.get('/help',(req,res)=>{
+    res.render(viewHelpFile,{
+        title: 'Help',
+        helpText:'Need some help?',
+    });
+});
 app.get('/help',(req,res)=>{
     res.send([{
         name:'TienDuy',
